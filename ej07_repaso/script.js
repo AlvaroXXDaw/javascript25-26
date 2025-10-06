9224ae7b72ce94164aa4f61bc2a0e36e59417002
AlvaroXXDaw <alvaro.alvaro601@gmail.com>
2025-10-06 11:39:49 +0200

    Guardando mis cambios antes del pull

--- ej07_repaso/script.js
+++ ej07_repaso/script.js
@@ -7,3 +7,28 @@ Crea un documento HTML que solicite cuántos caramelos tienes
  y te sobran __ caramelos".
 */
 
+
+let caja = document.querySelector("#numCaramelosInput");
+let personas = document.querySelector("#numPersonasInput");
+let boton = document.querySelector("button.btn-primary");
+let resultado = document.querySelector("#resultadoTA");
+
+
+boton.addEventListener("click",function()
+{
+    if(isNaN(caja.value) || isNaN(personas.value))
+{
+resultado.textContent = "Añada un numero entero";
+
+}   
+     else
+{
+        let operacion = Math.floor(caja.value) / Math.floor(personas.value);
+        resultado.textContent = operacion;
+}
+
+
+
+
+
+})
